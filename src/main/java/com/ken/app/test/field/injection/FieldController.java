e898da9ed5405a6311c53389856a4ecbfbbf537c
ken0901 <ken5260715@gmail.com>
2023-09-26 22:14:24 -0700

    Field Injection

---
+++ src/main/java/com/ken/app/test/field/injection/FieldController.java
@@ -0,0 +1,20 @@
+package com.ken.app.test.field.injection;
+
+import com.ken.app.test.component.scan.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FieldController {
+
+    @Autowired
+    private Coach myCoach;
+
+    // no need for constructors or setters
+
+    @GetMapping("/field")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
