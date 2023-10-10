3ba2a25ed090943227689ecd371ce988b6b7b5ee
ken0901 <ken5260715@gmail.com>
2023-10-09 20:22:28 -0700

    Spring boot REST: Get Single Employee

--- src/main/java/com/ken/app/test/api/rest/EmployeeRestController.java
+++ src/main/java/com/ken/app/test/api/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.ken.app.test.api.rest;
 import com.ken.app.test.api.entity.Employee;
 import com.ken.app.test.api.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    // Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw  new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
