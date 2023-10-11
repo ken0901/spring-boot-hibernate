c9c2d8f9cbdd962a8ac4a3d537ee262c47ce239e
ken0901 <ken5260715@gmail.com>
2023-10-10 21:23:56 -0700

    Spring boot REST: Spring Data REST configs and sorting

--- src/main/java/com/ken/app/test/api/dao/EmployeeRepository.java
+++ src/main/java/com/ken/app/test/api/dao/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.ken.app.test.api.dao;
 
 import com.ken.app.test.api.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // That's it ... no need to write any code.
 }
