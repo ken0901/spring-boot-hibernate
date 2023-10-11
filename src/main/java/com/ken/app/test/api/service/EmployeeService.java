cc2c2d882ca3975b745a2bf68b28c45f93b4afa5
ken0901 <ken5260715@gmail.com>
2023-10-10 21:35:23 -0700

    Spring boot REST API - Security

--- src/main/java/com/ken/app/test/api/service/EmployeeService.java
+++ src/main/java/com/ken/app/test/api/service/EmployeeService.java
@@ -1,16 +1,16 @@
-//package com.ken.app.test.api.service;
-//
-//import com.ken.app.test.api.entity.Employee;
-//
-//import java.util.List;
-//
-//public interface EmployeeService {
-//
-//    List<Employee> findAll();
-//
-//    Employee findById(int theId);
-//
-//    Employee save(Employee theEmployee);
-//
-//    void deleteById(int theId);
-//}
+package com.ken.app.test.api.service;
+
+import com.ken.app.test.api.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
--- src/main/java/com/ken/app/test/api/service/EmployeeServiceImpl.java
+++ src/main/java/com/ken/app/test/api/service/EmployeeServiceImpl.java
@@ -1,54 +1,54 @@
-//package com.ken.app.test.api.service;
-//
-//import com.ken.app.test.api.dao.EmployeeRepository;
-//import com.ken.app.test.api.entity.Employee;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
+package com.ken.app.test.api.service;
+
+import com.ken.app.test.api.dao.EmployeeRepository;
+import com.ken.app.test.api.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
 //@Service
-//public class EmployeeServiceImpl implements EmployeeService{
-//
-//    //private EmployeeDAO employeeDAO;
-//
-//    private EmployeeRepository employeeRepository;
-//
-//    @Autowired
-//    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
-//        employeeRepository = theEmployeeRepository;
-//    }
-//
-//    @Override
-//    public List<Employee> findAll() {
-//        return employeeRepository.findAll();
-//    }
-//
-//    @Override
-//    public Employee findById(int theId) {
-//        Optional<Employee> result = employeeRepository.findById(theId);
-//
-//        Employee theEmployee;
-//
-//        if(result.isPresent()){
-//            theEmployee = result.get();
-//        }else{
-//            // We didn't find the employee
-//            throw new RuntimeException("Did not find employee id - " + theId);
-//        }
-//
-//        return theEmployee;
-//    }
-//
-//    @Override
-//    public Employee save(Employee theEmployee) {
-//        return employeeRepository.save(theEmployee);
-//    }
-//
-//    @Override
-//    public void deleteById(int theId) {
-//        employeeRepository.deleteById(theId);
-//    }
-//}
+public class EmployeeServiceImpl implements EmployeeService{
+
+    //private EmployeeDAO employeeDAO;
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
+        employeeRepository = theEmployeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee;
+
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }else{
+            // We didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
