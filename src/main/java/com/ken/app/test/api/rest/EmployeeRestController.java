f12ac5a884d98055bb440548073d4d2debb6942b
ken0901 <ken5260715@gmail.com>
2023-10-11 20:55:23 -0700

    Spring boot REST API Security - Basic Configuration

--- src/main/java/com/ken/app/test/api/rest/EmployeeRestController.java
+++ src/main/java/com/ken/app/test/api/rest/EmployeeRestController.java
@@ -1,69 +1,69 @@
-//package com.ken.app.test.api.rest;
-//
-//import com.ken.app.test.api.entity.Employee;
-//import com.ken.app.test.api.service.EmployeeService;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class EmployeeRestController {
-//
-//    private EmployeeService employeeService;
-//
-//    public EmployeeRestController(EmployeeService theEmployeeService){
-//        employeeService = theEmployeeService;
-//    }
-//
-//    // Expose "/employees" and return a list of employees
-//    @GetMapping("/employees")
-//    public List<Employee> findAll(){
-//        return employeeService.findAll();
-//    }
-//
-//    // Add mapping for GET /employees/{employeeId}
-//    @GetMapping("/employees/{employeeId}")
-//    public Employee getEmployee(@PathVariable int employeeId){
-//        Employee theEmployee = employeeService.findById(employeeId);
-//
-//        if(theEmployee == null){
-//            throw  new RuntimeException("Employee id not found - " + employeeId);
-//        }
-//
-//        return theEmployee;
-//    }
-//
-//    // Add mapping for POST /employees - add new employee
-//    @PostMapping("/employees")
-//    public Employee addEmployee(@RequestBody Employee theEmployee){
-//        // Also, just in case they pass an id in JSON ... set id to 0
-//        // This is to force a save of new item ... instead of update
-//
-//        theEmployee.setId(0);
-//
-//        return employeeService.save(theEmployee);
-//    }
-//
-//    // Add mapping for PUT /employees - update existing employee
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee theEmployee){
-//
-//        return employeeService.save(theEmployee);
-//    }
-//
-//    // Add mapping for DELETE /employees/{employeeId} - delete employee
-//    @DeleteMapping("/employees/{employeeId}")
-//    public String deleteEmployee(@PathVariable int employeeId){
-//        Employee tempEmployee = employeeService.findById(employeeId);
-//
-//        // throw exception if null
-//        if(tempEmployee == null){
-//            throw new RuntimeException("Employee id not found - " + employeeId);
-//        }
-//
-//        employeeService.deleteById(employeeId);
-//
-//        return "Deleted employee id - " + employeeId;
-//    }
-//}
+package com.ken.app.test.api.rest;
+
+import com.ken.app.test.api.entity.Employee;
+import com.ken.app.test.api.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    // Expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    // Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw  new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    // Add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        // Also, just in case they pass an id in JSON ... set id to 0
+        // This is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        return employeeService.save(theEmployee);
+    }
+
+    // Add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        return employeeService.save(theEmployee);
+    }
+
+    // Add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+}
--- src/main/java/com/ken/app/test/api/service/EmployeeServiceImpl.java
+++ src/main/java/com/ken/app/test/api/service/EmployeeServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-//@Service
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     //private EmployeeDAO employeeDAO;
