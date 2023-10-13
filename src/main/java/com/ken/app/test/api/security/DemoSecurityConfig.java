f5f80a3bcbaf38f933d2d0fd5163a30d12b379b4
ken0901 <ken5260715@gmail.com>
2023-10-12 22:03:15 -0700

    Spring boot REST API Security - Custom Tables with BCrypt

--- src/main/java/com/ken/app/test/api/security/DemoSecurityConfig.java
+++ src/main/java/com/ken/app/test/api/security/DemoSecurityConfig.java
@@ -18,7 +18,18 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+        // Define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
