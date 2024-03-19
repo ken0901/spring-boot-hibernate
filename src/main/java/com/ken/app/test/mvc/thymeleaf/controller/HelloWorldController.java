b643965d7159e47924e58eb3bb7bf4e3385211ec
ken0901 <ken5260715@gmail.com>
2024-03-18 21:47:35 -0700

    Spring boot Spring MVC - GetMapping and PostMapping

--- src/main/java/com/ken/app/test/mvc/thymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/ken/app/test/mvc/thymeleaf/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.ken.app.test.mvc.thymeleaf.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +14,8 @@ import java.util.Locale;
 public class HelloWorldController {
 
     // New a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    //@RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -44,7 +47,9 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    //@RequestMapping("/processFormVersionThree")
+    //@GetMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
 
         // convert the data to all caps
