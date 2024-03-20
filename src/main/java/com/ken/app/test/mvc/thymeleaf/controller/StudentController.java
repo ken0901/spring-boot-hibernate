4d5d597bf2196bc4fab3e4172bf5dfaa6ffb50c9
ken0901 <ken5260715@gmail.com>
2024-03-19 20:53:18 -0700

    Spring boot Spring MVC - Form data binding text fields

---
+++ src/main/java/com/ken/app/test/mvc/thymeleaf/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.ken.app.test.mvc.thymeleaf.controller;
+
+import com.ken.app.test.mvc.thymeleaf.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+
+        // create a student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+        // log the input data
+        System.out.println("theStudent: "+theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+}
