a8c4d210a032ec7f914d85296f5bd3b21b11f348
ken0901 <ken5260715@gmail.com>
2024-03-20 21:33:49 -0700

    Spring boot Spring MVC - Validation @InitBinder

--- src/main/java/com/ken/app/test/mvc/validation/controller/CustomerController.java
+++ src/main/java/com/ken/app/test/mvc/validation/controller/CustomerController.java
@@ -2,15 +2,27 @@ package com.ken.app.test.mvc.validation.controller;
 
 import com.ken.app.test.mvc.validation.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/customerForm")
     public String showForm(Model theModel){
