ea782f89ec522d1070564f51867fd18101356cec
ken0901 <ken5260715@gmail.com>
2024-03-21 21:14:33 -0700

    Spring boot Spring MVC - Validation Regular Expressions

--- src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
+++ src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
@@ -1,9 +1,6 @@
 package com.ken.app.test.mvc.validation.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -16,6 +13,9 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -39,5 +39,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
