3759358885cf1985cb78bd22ec9d4df0dd33b31b
ken0901 <ken5260715@gmail.com>
2024-03-21 21:07:36 -0700

    Spring boot Spring MVC - Validation Number Range

--- src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
+++ src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
@@ -1,5 +1,7 @@
 package com.ken.app.test.mvc.validation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     @NotNull(message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +31,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
