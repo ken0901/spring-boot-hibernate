79106d2901db46b0064c389f00aaa98240897a23
ken0901 <ken5260715@gmail.com>
2024-03-21 21:19:52 -0700

    Spring boot Spring MVC - Validation Make Integer Fields Required

--- src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
+++ src/main/java/com/ken/app/test/mvc/validation/model/Customer.java
@@ -12,7 +12,8 @@ public class Customer {
 
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    @NotNull(message = "is required")
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -33,11 +34,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
