5a0f6496fb0202155932d08a5738bcaf889ef4dc
ken0901 <ken5260715@gmail.com>
2024-03-22 21:43:50 -0700

    Spring Boot - Spring MVC CRUD Get Employees

---
+++ src/main/java/com/ken/app/test/mvc/crud/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.ken.app.test.mvc.crud.controller;
+
+import com.ken.app.test.mvc.crud.entity.Employee;
+import com.ken.app.test.mvc.crud.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+        // get the employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}
