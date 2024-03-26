2988a3f24c5d4604676012bee630930cfee16ba5
ken0901 <ken5260715@gmail.com>
2024-03-25 20:28:46 -0700

    Spring Boot - Spring MVC CRUD Add Employees

--- src/main/java/com/ken/app/test/mvc/crud/controller/EmployeeController.java
+++ src/main/java/com/ken/app/test/mvc/crud/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.ken.app.test.mvc.crud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,5 +30,24 @@ public class EmployeeController {
         theModel.addAttribute("employees", theEmployees);
 
         return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee",theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployees(@ModelAttribute("employee") Employee theEmployee){
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
     }
 }
--- src/main/java/com/ken/app/test/mvc/crud/dao/EmployeeRepository.java
+++ src/main/java/com/ken/app/test/mvc/crud/dao/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.ken.app.test.mvc.crud.dao;
 import com.ken.app.test.mvc.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // That's it ... no need to write any code.
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
 
