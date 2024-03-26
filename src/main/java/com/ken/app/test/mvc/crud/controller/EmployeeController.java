ce202f048fbb3fcca4cfceba5fb338b9d5b4ef5d
ken0901 <ken5260715@gmail.com>
2024-03-25 20:40:54 -0700

    Spring Boot - Spring MVC CRUD Update Employees

--- src/main/java/com/ken/app/test/mvc/crud/controller/EmployeeController.java
+++ src/main/java/com/ken/app/test/mvc/crud/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.ken.app.test.mvc.crud.entity.Employee;
 import com.ken.app.test.mvc.crud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,5 +46,17 @@ public class EmployeeController {
 
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to our form
+        return "employees/employee-form";
     }
 }
