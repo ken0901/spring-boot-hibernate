ec1c5403e3ad4b913c79510b91cb050ef540dcba
ken0901 <ken5260715@gmail.com>
2024-03-26 21:47:12 -0700

    Spring Boot - Spring MVC Security - Custom Login Form

--- src/main/java/com/ken/app/test/mvc/security/controller/LoginController.java
+++ src/main/java/com/ken/app/test/mvc/security/controller/LoginController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String showHome(){
-        return "securityTemplate/home";
+    @GetMapping("/showLoginPage")
+    public String showLoginPage(){
+
+        return "securityTemplate/plain-login";
     }
 }
