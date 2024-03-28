16fa29cbe602e9a122a91ef3b2fb33077b0468dd
ken0901 <ken5260715@gmail.com>
2024-03-27 21:37:11 -0700

    Spring Boot - Spring MVC Security - Logout

--- src/main/java/com/ken/app/test/mvc/security/config/HomeSecurityConfig.java
+++ src/main/java/com/ken/app/test/mvc/security/config/HomeSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ken.app.test.mvc.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -43,6 +44,8 @@ public class HomeSecurityConfig {
                             .loginPage("/showLoginPage")
                             .loginProcessingUrl("/authenticateTheUser")
                             .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
                 );
         return http.build();
     }
