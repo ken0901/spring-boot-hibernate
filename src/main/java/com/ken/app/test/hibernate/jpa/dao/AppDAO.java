ba29eede55b0558cc9e1ff3b616657e996335b76
ken0901 <ken5260715@gmail.com>
2024-03-29 20:49:26 -0700

    Spring Boot - JPA/Hibernate - @OneToOne Mapping entity Instructor and DAO

---
+++ src/main/java/com/ken/app/test/hibernate/jpa/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.ken.app.test.hibernate.jpa.dao;
+
+import com.ken.app.test.hibernate.jpa.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+}
---
+++ src/main/java/com/ken/app/test/hibernate/jpa/dao/AppDAOImpl.java
@@ -0,0 +1,24 @@
+package com.ken.app.test.hibernate.jpa.dao;
+
+import com.ken.app.test.hibernate.jpa.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
