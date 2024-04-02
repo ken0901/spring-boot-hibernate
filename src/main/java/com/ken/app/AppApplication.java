af6b789493c409bd9fa6bdf99bab06bb1f4110cd
ken0901 <ken5260715@gmail.com>
2024-04-01 21:33:17 -0700

    Spring Boot - JPA/Hibernate - @OneToMany Fetch types: eager vs lazy

--- src/main/java/com/ken/app/AppApplication.java
+++ src/main/java/com/ken/app/AppApplication.java
@@ -38,8 +38,21 @@ public class AppApplication {
 
 			// deleteInstructorDetail(appDAO);
 
-			createInstructorWithCourses(appDAO);
+			// createInstructorWithCourses(appDAO);
+
+			findInstructorWithCourses(appDAO);
 		};
+	}
+
+	private void findInstructorWithCourses(AppDAO appDAO) {
+		int theId = 3;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+		System.out.println("Done!");
 	}
 
 	private void createInstructorWithCourses(AppDAO appDAO) {
--- src/main/java/com/ken/app/test/hibernate/jpa/entity/Instructor.java
+++ src/main/java/com/ken/app/test/hibernate/jpa/entity/Instructor.java
@@ -30,7 +30,9 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
+    @OneToMany(mappedBy = "instructor",
+                fetch = FetchType.EAGER,
+                cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
     private List<Course> courses;
 
     // create constructors
