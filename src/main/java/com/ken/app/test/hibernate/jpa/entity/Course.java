11b2352a1c76372b6666f4066864f1c91f8811d4
ken0901 <ken5260715@gmail.com>
2024-04-03 21:17:20 -0700

    Spring Boot - JPA/Hibernate - @ManyToMany - Map course to student and student to course

--- src/main/java/com/ken/app/test/hibernate/jpa/entity/Course.java
+++ src/main/java/com/ken/app/test/hibernate/jpa/entity/Course.java
@@ -25,6 +25,14 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY ,cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     public Course() {
     }
 
@@ -62,6 +70,22 @@ public class Course {
 
     public void setReviews(List<Review> reviews) {
         this.reviews = reviews;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    // add a convenience method
+    public void addStudent(Student theStudent){
+        if(students == null){
+            students = new ArrayList<>();
+        }
+        students.add(theStudent);
     }
 
     // add a convenience method
--- src/main/java/com/ken/app/test/hibernate/jpa/entity/Student.java
+++ src/main/java/com/ken/app/test/hibernate/jpa/entity/Student.java
@@ -2,6 +2,9 @@ package com.ken.app.test.hibernate.jpa.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -19,6 +22,14 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY ,cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> courses;
 
     public Student() {
     }
@@ -59,6 +70,22 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add a convenience method
+    public void addCourse(Course theCourse){
+        if(courses == null){
+            courses = new ArrayList<>();
+        }
+        courses.add(theCourse);
     }
 
     @Override
